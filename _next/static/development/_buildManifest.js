e9f06bf0de5001c8c35bbd9c83cc4ab348f877fb
David <david@krawaller.se>
2021-05-16 09:43:01 +0200

    deploy 2021-05-16  9:42

---
+++ _next/static/development/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/games/amazons":["static\u002Fchunks\u002Fpages\u002Fgames\u002Famazons.js"],sortedPages:["\u002F_app","\u002F_error","\u002Fgames\u002Famazons"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
